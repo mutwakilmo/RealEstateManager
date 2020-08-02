250bc0268f299d625458545fc96754f34736c68e
mutwakilmo <mutwakilmo@gmail.com>
2020-08-02 19:57:32 +0200

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.openclassrooms.realestatemanager
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT: Long = 10000 //3 second
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            //This method will be executed once the timer is over
+            startActivity(Intent(this, MainActivity::class.java))
+            //close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
